36333c41daa46649954f760224a4a27c9686db2a
rajprajapati2001 <rp5876907@gmail.com>
2026-04-22 20:10:56 +0530

    ec2

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <body style="background-color: green; color: white; font-family: sans-serif;">
+      <h1>Hello World</h1>
+      <p>I am doing CI/CD in GitHub Actions and connecting it with AWS EC2 Ubuntu</p>
+    </body>
+  `);
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'Online' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
